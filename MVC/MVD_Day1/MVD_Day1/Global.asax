1c0d5819cd3f636e687dfa2b350323bc4e330716
BanurekhaV <rekhasairam@hotmail.com>
2022-08-01 13:46:54 +0530

    MVC

---
+++ MVC/MVD_Day1/MVD_Day1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVD_Day1.MvcApplication" Language="C#" %>
---
+++ MVC/MVD_Day1/MVD_Day1/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVD_Day1.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Deptname { get; set; }
+    }
+}
