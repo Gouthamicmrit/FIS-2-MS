4ab0a614dddf6834f940eb24bc8555582420c84b
BanurekhaV <rekhasairam@hotmail.com>
2022-08-03 16:17:56 +0530

    CoreEF

---
+++ Core/WebApp_Core_EF/WebApp_Core_EF/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_Core_EF.Models;
+
+namespace WebApp_Core_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private const string employeecache = "employeelist";
+        private readonly IEmployeeRepository _datarepository;
+        private IMemoryCache _cache;
+        private ILogger<EmployeeController> _logger;
+
+        //constructor
+        public EmployeeController(IEmployeeRepository erepo,IMemoryCache cache,ILogger<EmployeeController> logger)
+        {
+            _datarepository = erepo;
+            _cache = cache;
+            _logger = logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAllEmployees()
+        {
+            _logger.Log(LogLevel.Information, "Trying to Fetch the List of Employees from Cache");
+            if(_cache.TryGetValue(employeecache,out IEnumerable<Employee> employees))
+            {
+                _logger.Log(LogLevel.Information, "Employees list found in the cache");
+            }
+            else
+            {
+                _logger.Log(LogLevel.Information, "Employee List not found in the cache.., hence fetching from the database");
+                employees = _datarepository.GetAllEmployees();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(3000))
+                    .SetPriority(CacheItemPriority.Normal)
+                    .SetSize(1024);
+                _cache.Set(employeecache, cacheEntryOptions);
+            }
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ Core/WebApp_Core_EF/WebApp_Core_EF/Migrations/20220803095606_initial-create.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp_Core_EF.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblEmployee",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<float>(type: "real", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblEmployee", x => x.Eid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblEmployee");
+        }
+    }
+}
