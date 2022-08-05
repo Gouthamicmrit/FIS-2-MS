c653f6f3cb842a65a5a40c83239316af58da16b2
BanurekhaV <rekhasairam@hotmail.com>
2022-08-05 13:54:21 +0530

    BFF

---
+++ Microservices/ProductMicroServices/CustomerMicroServices/CustomerDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CustomerMicroServices
+{
+    public class CustomerDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [JsonIgnore(Condition=JsonIgnoreCondition.WhenWritingNull)]
+        public string Address { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string Phone { get; set; }
+    }
+}
